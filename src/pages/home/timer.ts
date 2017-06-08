5a3ae17c4411a69c1a5ed1a4251eede416008bb3
cody <cody.backhaus>
2017-06-08 13:08:25 -0500

    General flow changes -Cordova & Ionic upgrade -commented on timer process -changes the show animation logic -made chest always available after 1hr -temporarily reduced timer to 3 min -removed timer reset event listener on homepage and handled that with timer logic

--- src/pages/home/timer.ts
+++ src/pages/home/timer.ts
@@ -15,24 +15,24 @@ export class TimerComponent {
     public runningTimer: any;
 
     constructor(public platform: Platform, public events: Events, private localNotifications: LocalNotifications) {
-        events.subscribe('resumeTimer', () => {
+        events.subscribe('resumeTimer', () => { //resume timer event should initialize the timer.
             this.initTimer();
         })
 
-        events.subscribe('timerReset', () => {
+        events.subscribe('timerReset', () => { //resets the timer to attribute on timer element (home.html)
             localStorage.setItem("timer", this.timeInSeconds.toString());
             this.schedulePush();
             this.initTimer();
         })
 
-        platform.pause.subscribe(() => {
+        platform.pause.subscribe(() => { //store time app is paused
             this.pauseTimer();
             var currentSeconds = new Date().getTime() / 1000;
             localStorage.setItem("appPausedTime", currentSeconds.toString());
         });
     }
 
-    ngOnInit() {
+    ngOnInit() { //on application start
         this.schedulePush();
         this.initTimer();
     }
@@ -42,17 +42,21 @@ export class TimerComponent {
     }
 
     initTimer() {
+        //if timeInSeconds (set on home.html timer element) isn't valid, set to 0
         if (!this.timeInSeconds) {
             this.timeInSeconds = 0;
         }
 
-        if (localStorage.getItem("firstStart") == "true") { //detect first start and then set timer to 0 to trigger a treasure chest
+        //detect first start and then set timer to 0 to trigger a treasure chest
+        if (localStorage.getItem("firstStart") == "true") {
             localStorage.setItem("timer", "0");
             localStorage.setItem("firstStart", "false");
         }
 
+        //get time remaining if there is one
         var timeRemaining = parseInt(localStorage.getItem("timer"));
 
+        //initialize timer
         this.timer = <ITimer>{
             seconds: this.timeInSeconds,
             runTimer: false,
@@ -61,33 +65,30 @@ export class TimerComponent {
             secondsRemaining: this.timeInSeconds
         };
 
-        // var currentSeconds = new Date().getTime() / 1000;
-        // var pausedTime = parseInt(localStorage.getItem("appPausedTime"));
-
-        // if (pausedTime > 0) {
-        //     timeRemaining = (timeRemaining - (currentSeconds - pausedTime));
-        //     localStorage.removeItem("appPausedTime");
-        // }
-
+        //This is to find the time remaining after the app has been paused or killed
+        //if we don't have a start time for the timer, set one
         if (localStorage.getItem("appTimerStart") == "null") {
             localStorage.setItem("appTimerStart", (new Date().getTime() / 1000).toString());
-        } else {
+        } else { //otherwise get that start time and calculate time remaining
             var currentSeconds = new Date().getTime() / 1000;
-            var appStart = parseInt(localStorage.getItem("appTimerStart"));        
+            var appStart = parseInt(localStorage.getItem("appTimerStart"));
             timeRemaining = (this.timeInSeconds - (currentSeconds - appStart));
         }
 
+        //if the time remaining after app resume is greater than zero, run the timer
         if (timeRemaining > 0) {
             this.timer.seconds = timeRemaining;
             this.timer.secondsRemaining = timeRemaining;
             this.timer.runTimer = true;
-        } else {
+        } else { //else, kill the timer/mark as finished
             this.timer.seconds = 0;
             this.timer.secondsRemaining = 0;
             this.timer.hasFinished = true;
         }
 
+        //set the timer display
         this.timer.displayTime = this.getSecondsAsDigitalClock(this.timer.secondsRemaining);
+        //run the timer
         this.startTimer();
     }
 
@@ -107,24 +108,29 @@ export class TimerComponent {
     }
 
     timerTick() {
+        //tick the timer down by second using a timeout
         this.runningTimer = setTimeout(() => {
             if (!this.timer.runTimer) { return; }
-            this.timer.secondsRemaining--;
+            this.timer.secondsRemaining--; //decrement timer
             this.timer.displayTime = this.getSecondsAsDigitalClock(this.timer.secondsRemaining);
-            if (this.timer.secondsRemaining > 0) {
+            if (this.timer.secondsRemaining > 0) { //if theres time left
+                //recursively call this function to decrement the time
                 this.timerTick();
+                //store the time in the event the app is paused
                 localStorage.setItem("timer", this.timer.secondsRemaining.toString());
             }
-            else {
-                localStorage.setItem("timer", "0");
+            else { //if theres no time left, kill the timer
+                localStorage.setItem("timer", "0"); 
                 this.timer.hasFinished = true;
                 this.events.publish("timerFinished");
+                localStorage.setItem("treasureAvailable","true"); //set treasure to available
                 localStorage.setItem("appTimerStart", null);
+                this.events.publish("timerReset"); //reset the timer so the user gets a prize in another hour.
             }
-        }, 1000);
+        }, 1000); //1 second
     }
 
-    getSecondsAsDigitalClock(inputSeconds: number) {
+    getSecondsAsDigitalClock(inputSeconds: number) { //formatting the time as HH:MM:SS
         var sec_num = parseInt(inputSeconds.toString(), 10); // don't forget the second param
         var hours = Math.floor(sec_num / 3600);
         var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
@@ -141,7 +147,7 @@ export class TimerComponent {
         return hoursString + ':' + minutesString + ':' + secondsString;
     }
 
-    schedulePush() {
+    schedulePush() { //schedule a local notification to alert the user to a new rewarrrdddd
         this.localNotifications.schedule({
             text: 'You have loot to open from Zymo!',
             at: new Date(new Date().getTime() + (this.timeInSeconds * 1000)),
