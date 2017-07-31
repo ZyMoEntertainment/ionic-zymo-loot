7b4c6f1a1102a4516eb85edb52e6a7da127b3ef9
cody <cody.backhaus>
2017-07-31 12:07:36 -0500

    added ui for holograms, transitions

--- src/pages/hologram/hologram.ts
+++ src/pages/hologram/hologram.ts
@@ -7,8 +7,18 @@ import { Firebase } from "@ionic-native/firebase";
   templateUrl: 'hologram.html'
 })
 export class HologramPage {
+  hologramUrl: string;
+  currentHologramIdx: number;
+  urls: Array<string>;
+  animateHolo: boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private firebase: Firebase) {
-      this.firebase.setScreenName("hologram_view")
+      this.firebase.setScreenName("hologram_view");
+      this.animateHolo = false;
+      this.urls = ["assets/images/hologram_360.gif", "assets/images/jelly.gif"];
+      let random_num: number = (Math.round(Math.random()))
+      this.currentHologramIdx = random_num;
+      this.assignUrl(this.currentHologramIdx);
   }
 
   goBack() {
@@ -19,4 +29,38 @@ export class HologramPage {
   logEvent(eventName:string, eventBundle:object){
       this.firebase.logEvent(eventName, eventBundle);
   }
+
+  assignUrl(idx:number){
+    this.hologramUrl = this.urls[idx];
+  }
+
+  nextHolo() {
+    if(this.currentHologramIdx == this.urls.length-1) {
+      this.currentHologramIdx = 0;
+    }
+    else{
+      this.currentHologramIdx += 1;
+    }
+    this.animateHologram();
+  }
+
+  prevHolo() {
+    if(this.currentHologramIdx == 0) {
+      this.currentHologramIdx = this.urls.length-1;
+    }
+    else{
+      this.currentHologramIdx -= 1;
+    }
+    this.animateHologram();
+  }
+
+  animateHologram() {
+    this.animateHolo = true;
+    setTimeout(() => {
+      this.assignUrl(this.currentHologramIdx);
+      this.animateHolo = false;
+    }, 700
+    );
+  }
+
 }
